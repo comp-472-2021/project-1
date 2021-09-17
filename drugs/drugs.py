a111f523c34ac4604deb2299718cb527bde1bbdc
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2021-09-17 18:19:47 -0400

    test

--- drugs/drugs.py
+++ drugs/drugs.py
@@ -14,7 +14,7 @@ def fetch_drugs_target_data(drugs_csv_model):
     return drugs_csv_model[DRUGS_FEATURES["DRUG"]].head()
 
 
-def get_naive_bayes_classifier():
+def get_naive_bayes_classifier(drugs_csv_model, features_vectors):
     initial_naive_bayes_classifier = MultinomialNB(alpha=.01)
     drugs_target_data = fetch_drugs_target_data(drugs_csv_model)
     initial_naive_bayes_classifier.fit(features_vectors, drugs_target_data)
@@ -27,7 +27,7 @@ vectorizer = TfidfVectorizer()
 drugs_features = fetch_drugs_features_data(drugs_csv_model)
 features_vectors = vectorizer.fit_transform(drugs_features)
 
-naive_bayes_classifier = get_naive_bayes_classifier()
+naive_bayes_classifier = get_naive_bayes_classifier(drugs_csv_model, features_vectors)
 
 # testing
 print_prediction(naive_bayes_classifier, vectorizer)
