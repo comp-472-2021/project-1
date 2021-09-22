452307c6c56b9002b44ef8420668d708d627d872
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2021-09-22 17:19:15 -0400

    question_4

--- drugs/drugs.py
+++ drugs/drugs.py
@@ -1,28 +1,11 @@
 import pandas as panda
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
 
-from constants import DRUGS_FEATURES
-from test import print_prediction
-from functions import plot_instances
+import questions.question_3 as question3
 
 drugs_csv_model = panda.read_csv('drug200.csv')
 
-
 # Question 3
-
-drug_column_counts = drugs_csv_model.Drug.value_counts()
-
-drugA_count = drug_column_counts.drugA
-drugB_count = drug_column_counts.drugB
-drugC_count = drug_column_counts.drugC
-drugX_count = drug_column_counts.drugX
-drugY_count = drug_column_counts.drugY
-
-drug_names = sorted(drugs_csv_model['Drug'].unique())
-drug_values = [drugA_count, drugB_count, drugC_count, drugX_count, drugY_count]
-drug_pdf = 'drug-distribution.pdf'
-plot_instances(drug_pdf, drug_names, drug_values)
+question3.question3()
 
 # Question 4
 
