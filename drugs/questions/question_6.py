36075de955c5a96a15d58f6acf4252e351ee87ee
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2021-09-23 19:22:45 -0400

    drugs-question-6

--- drugs/questions/question_6.py
+++ drugs/questions/question_6.py
@@ -1,7 +1,6 @@
 from sklearn import metrics
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
@@ -24,14 +23,15 @@ def decision_tree(features_train_set, target_train_set, features_test_set, targe
 
 
 def grid_search_tree(features_train_set, target_train_set, features_test_set, target_test_set):
-    estimator = SVC(gamma='scale')
-    grid_search_tree_classifier = GridSearchCV(DecisionTreeClassifier(),
-                                               param_grid={"criterion": ["entropy"]})
+    param_grid = {"criterion": ["gini"],
+                  'max_depth': list(range(1, 15)),
+                  'min_samples_split': list(range(5, 15))
+                  }
+    grid_search_tree_classifier = GridSearchCV(DecisionTreeClassifier(), param_grid)
     grid_search_tree_classifier.fit(features_train_set, target_train_set)
 
     # testing
-    prediction_results = grid_search_tree_classifier.predict(features_test_set)
-    print(metrics.accuracy_score(target_test_set, prediction_results))
+    print(grid_search_tree_classifier.best_params_)
 
 
 def perceptron(features_vectors, target_train_set, vectorizer, features_test_set):
